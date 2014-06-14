d181481ac8eefc6c452b27313192a18ad1da8f6b
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-14 20:32:31 +0100

    Added main.rb

---
+++ app/main.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require_relative '../lib/ship'
+require_relative '../lib/ships'
+require_relative '../lib/coordinate'
+require_relative '../lib/coordinates'
+
+# set :views, Proc.new {File.join(root, '..', "views")}
+# set :public, Proc.new {File.join(root, '..', "public")}
+
+enable :sessions
+
+get '/' do
+	session[:player_count] = 0
+	erb :index
+end
+
+post '/player_login' do
+	erb :player_login
+end
+
+get '/battlefield' do
+	erb :battlefield
+end
