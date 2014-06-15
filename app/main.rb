67b0705d0ab9e2fb3380a720355238231c36654e
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-15 15:18:00 +0100

    Minor changes

--- app/main.rb
+++ app/main.rb
@@ -4,20 +4,15 @@ require_relative '../lib/ships'
 require_relative '../lib/coordinate'
 require_relative '../lib/coordinates'
 
-# set :views, Proc.new {File.join(root, '..', "views")}
-# set :public, Proc.new {File.join(root, '..', "public")}
-
+enable :sessions
 use Rack::Session::Pool, :expire_after => 60
 
-enable :sessions
+# set :views, Proc.new {File.join(root, '..', "views")}
+# set :public, Proc.new {File.join(root, '..', "public")}
 
 get '/' do
 	session[:player_count] = 0
 	erb :index
-end
-
-post '/player_login' do
-	erb :player_login
 end
 
 get '/battlefield' do
