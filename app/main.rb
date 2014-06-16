5f71451e9ac4e657bfce4a79502ed8cf025c3477
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-16 11:35:49 +0100

    Changed the board from using div to canvas

--- app/main.rb
+++ app/main.rb
@@ -5,7 +5,7 @@ require_relative '../lib/coordinate'
 require_relative '../lib/coordinates'
 
 enable :sessions
-#use Rack::Session::Pool, :expire_after => 60
+use Rack::Session::Pool, :expire_after => 60
 
 # set :views, Proc.new {File.join(root, '..', "views")}
 # set :public, Proc.new {File.join(root, '..', "public")}
@@ -17,15 +17,19 @@ end
 post '/login' do
 	session[:player_name]= params[:name]
 	unless session[:player_name].empty?
-		redirect '/battlebase'		
+		redirect '/battlefield'		
 	else
-    	@message = "We don't accept unknown human into the battlefield!"
+    	@message = "No unknown human is allowed at battlefield"
     	erb :index
   	end
 end
 
 get '/battlebase' do
 	erb :battlebase
+end
+
+get '/battlefield' do
+	erb :battlefield
 end
 
 get '/login' do
